cc7093ba320c5900c93a2e100b6e6fcee8276366
AlinaUrOk <71850840+AlinaUrOk@users.noreply.github.com>
2020-12-01 11:25:30 +0300

    Add files via upload

---
+++ Laba 4.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+struct triplet
+{
+	string key;
+	string value;
+	bool end;
+
+	void Set(string key, string value, bool end)
+	{
+		this->key = key;
+		this->value = value;
+		this->end = end;
+	}
+
+	string Get_key() { return key; }
+
+	string Get_value() { return value; }
+
+	bool Get_end() { return end; }
+
+	int Get_key_size() { return key.size(); }
+};
+int i = -1;
+triplet search_rules(triplet answer[], string str)
+{
+	triplet creating;
+	string str_sub1, str_sub2;
+	i++;
+
+	if (str.find("->.") != string::npos)
+	{
+		str_sub1 = str.substr(0, str.find("->."));
+		str_sub2 = str.substr((str.find("->.") + 3), str.size());
+		creating.Set(str_sub1, str_sub2, true);
+		answer[i] = creating;
+		return *answer;
+
+	}
+	if (str.find("->") != string::npos)
+	{
+
+		str_sub1 = str.substr(0, str.find("->"));
+		str_sub2 = str.substr((str.find("->") + 2), str.size());
+		creating.Set(str_sub1, str_sub2, false);
+		answer[i] = creating;
+		return *answer;
+	}
+
+}
+void transformation(string str, triplet rules[])
+{
+	int length = i;
+	string str_sub1, str_sub2;
+	for (int i = 0; i < length; ++i) {
+		if (str.find(rules[i].Get_key()) != string::npos)
+		{
+
+			str_sub1 = str.substr(0, str.find(rules[i].Get_key()));
+			str_sub2 = str.substr((str.find(rules[i].Get_key()) + rules[i].Get_key_size()), str.size());
+			str = str_sub1 + rules[i].Get_value() + str_sub2;
+			cout << str << '\n';
+			if (rules[i].Get_end() == true) return;
+			i = -1;
+			continue;
+		}
+	}
+	return;
+}
+int main()
+{
+	ifstream file;
+	file.open("C:\\test.txt");
+	triplet* rules = new triplet[100];
+	string buf, str;
+
+	getline(file, str);
+	while (!file.eof())
+	{
+		buf = "";
+		getline(file, buf);
+		*rules = search_rules(rules, buf);
+	}
+	cout << str << endl;
+
+	transformation(str, rules);
+}
---
+++ mas.cpp
@@ -0,0 +1,90 @@
+﻿// mas.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+/*4)	Дано дві послідовності, елементами яких є цілі числа.
+Знайти максимальну спільну підпослідовність цих послідовностей
+(елементи не обов’язково мають бути розташовані підряд).*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+
+	int max = 0; int d = 0;
+	cout << "Enter amount of numbers for massive 1: " << endl;
+	int n;
+	cin >> n; '\n';
+	int* a = new int[n];
+    for (size_t i = 0; i < n; i++)
+    {
+        a[i] = rand() % 20;
+        cout << a[i] << ' ';
+    }
+    cout << endl;
+	cout << "Enter amount of numbers for massive 2: " << '\n';
+	int m;
+	cin >> m; '\n';
+	int* b = new int[m];
+    for (size_t i = 0; i < m; i++)
+    {
+        b[i] = rand() % 20;
+        cout << b[i] << ' ';
+    }
+
+    size_t l = 0;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        size_t count = 1;
+
+        for (size_t j = 0; j < i; j++)
+        {
+            if (a[j] == a[i]) ++count;
+        }
+
+        for (size_t j = 0; count != 0 && j < m; j++)
+        {
+            if (b[j] == a[i]) --count;
+        }
+
+        if (count == 0) ++l;
+    }
+
+    int* ab = new int[l];
+
+    size_t s = 0;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        size_t count = 1;
+
+        for (size_t j = 0; j < i; j++)
+        {
+            if (a[j] == a[i]) ++count;
+        }
+
+        for (size_t j = 0; count != 0 && j < m; j++)
+        {
+            if (b[j] == a[i]) --count;
+        }
+
+        if (count == 0) ab[s++] = a[i];
+    }
+    cout << endl;
+    cout << "The common elements: ";
+
+    for (size_t i = 0; i < l; i++)
+    {
+        cout << ab[i] << ' ';
+    }
+    cout << endl;
+
+    delete[] ab;
+	delete[] a;
+	delete[] b;
+	return 0;
+}
+
+
+
+
